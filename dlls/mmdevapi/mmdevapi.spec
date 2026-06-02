6bcd3631c8528a293ccd377f6d73fcdb5809f45c
Plusone Chiang <plusone@appx.com.tw>
2026-06-02 21:09:49 +0800

    add audio device rescan method

--- dlls/mmdevapi/mmdevapi.spec
+++ dlls/mmdevapi/mmdevapi.spec
@@ -27,3 +27,5 @@
 @ stdcall -private auxMessage(long long long long long)
 @ stdcall -private midMessage(long long long long long)
 @ stdcall -private modMessage(long long long long long)
+18 stdcall RescanAudioDevices()
+19 stdcall RescanAudioDevicesRundll(long long str long long)
--- dlls/mmdevapi/mmdevapi_private.h
+++ dlls/mmdevapi/mmdevapi_private.h
@@ -86,6 +86,9 @@ struct audio_client {
 
 extern HRESULT MMDevEnum_Create(REFIID riid, void **ppv);
 extern void MMDevEnum_Free(void);
+extern void start_notification_thread(void);
+extern HRESULT WINAPI RescanAudioDevices(void);
+extern void WINAPI RescanAudioDevicesRundll(HWND hwnd, HINSTANCE hinst, LPWSTR cmdline, int nCmdShow);
 
 typedef struct _DriverFuncs {
     HMODULE module;
